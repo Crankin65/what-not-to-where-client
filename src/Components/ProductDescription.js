1fe2cea8b15dd2aff8bf544aa44af641e6f3ff1e
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-07-06 02:16:01 -0400

    removed unescapted quotes

--- src/Components/ProductDescription.js
+++ src/Components/ProductDescription.js
@@ -4,14 +4,14 @@ export default function ProductDescription() {
 	return(
 		<div className='pt-16 mx-2'>
 			<div className=' flex flex-col bg-gray-800 font-bold'>
-				<p className='flex px-2 py-2'>Welcome to "What Not to Where!"</p>
+				<p className='flex px-2 py-2'>Welcome to &quot;What Not to Where!&quot;</p>
 			</div>
 
 				<br/>
 
 			<div className=' flex flex-col bg-gray-800 font-light px-2 py-2'>
 				<p>	A simple application that lets you review both basic and detailed information from three
-				separate weather APIs and gives you the ability to determine "What Not to Where" when
+				separate weather APIs and gives you the ability to determine &quot;What Not to Where&quot; when
 				you go outside! </p>
 			</div>
 
@@ -19,7 +19,7 @@ export default function ProductDescription() {
 
 
 			<div className=' flex flex-col bg-gray-800 font-light px-2 py-2'>
-				<p>This application is free to use but please let me know if there's any other features you'd like to see!</p>
+				<p>This application is free to use but please let me know if there&apos;s any other features you&apos;d like to see!</p>
 			</div>
 
 
