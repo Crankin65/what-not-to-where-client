b0991de03ed9a3a07a3d4567f94500e7feeb622c
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-07-06 02:25:39 -0400

    Updated fetch api to heroku site

--- src/API-Calls/pingAllWeatherAPIs.js
+++ src/API-Calls/pingAllWeatherAPIs.js
@@ -1,6 +1,8 @@
 export default async function pingAllWeatherAPIs(city){
 	if (city) {
-		const response = await fetch(`http://localhost:8000/get/weather/${city}`, {
+		// const response = await fetch(`http://localhost:8000/get/weather/${city}`, {
+		const response = await fetch(`https://express-backend-weather-server-a6efe7468e93.herokuapp.com/${city}`, {
+
 			method: 'GET'
 		});
 
