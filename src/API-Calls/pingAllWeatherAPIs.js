351202ac295941a1a39d0314986bb98d1e7e5073
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-07-06 02:27:32 -0400

    correct fetch api link

--- src/API-Calls/pingAllWeatherAPIs.js
+++ src/API-Calls/pingAllWeatherAPIs.js
@@ -1,7 +1,7 @@
 export default async function pingAllWeatherAPIs(city){
 	if (city) {
 		// const response = await fetch(`http://localhost:8000/get/weather/${city}`, {
-		const response = await fetch(`https://express-backend-weather-server-a6efe7468e93.herokuapp.com/${city}`, {
+		const response = await fetch(`https://express-backend-weather-server-a6efe7468e93.herokuapp.com/get/weather/${city}`, {
 
 			method: 'GET'
 		});
